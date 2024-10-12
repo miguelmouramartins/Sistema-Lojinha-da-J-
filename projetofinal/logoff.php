501953d7c6fe31390dca5cb9dbcc3bf8d450b434
miguelmouramartins <eloir.miguel.2612@gmail.com>
2024-10-12 17:58:47 -0300

    Delete projetofinal directory

--- projetofinal/logoff.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION['id'] = null;
-session_destroy();
-echo "Logout realizado com sucesso!";
-?>
--- projetofinal/sessao.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-ini_set("display_errors",0);
-$nome = $_SESSION['id'];
-if($nome == null){
-	die("Usuário não autenticado!
-	<a href='login.html'>Logar</a>");
-}
